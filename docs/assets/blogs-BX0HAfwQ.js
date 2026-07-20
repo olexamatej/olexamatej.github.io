977bc8cf87fecfabd7f087bd0229693c7ab972b6
olexamatej <olexa.matej@gmail.com>
2026-07-20 13:51:48 +0200

    fix: raise desktop typography minimums

---
+++ docs/assets/blogs-BX0HAfwQ.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as r}from"./index--d4h3iAW.js";import{W as o}from"./Writeups-CVYHwwns.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
