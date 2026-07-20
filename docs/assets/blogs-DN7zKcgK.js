c88d022eff2caab4626dfb19f7f88fb0202f6706
olexamatej <olexa.matej@gmail.com>
2026-07-20 13:49:00 +0200

    fix: scale portfolio for large displays

---
+++ docs/assets/blogs-DN7zKcgK.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as r}from"./index-DoCSfTyU.js";import{W as o}from"./Writeups-BAxvkN2D.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
